14e1cc256658676f92a280aff7dd1e4d45c6efed
Sarvesh Mishra <officialsmishra@gmail.com>
2022-05-02 18:57:06 +0530

    Starting Files

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/users", userRoute);
+app.get("/", (req, res) => {
+	res.send("Backend is running");
+});
 app.listen(port, () => {
 	new connectToDB();
 	console.log("Server is running on port 3000");
