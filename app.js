97e17d1e6be5b909b33f2070eecd6530653eb7d4
Sarvesh Mishra <officialsmishra@gmail.com>
2022-05-05 19:14:03 +0530

    update save image route

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const connectToDB = require("./database/mongodb");
 const userRoute = require("./routes/user.route");
 const facebookAuthRoute = require("./routes/facebookAuth.route");
+const savedImageRoute = require("./routes/saved_image.route");
 const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use("/users", userRoute);
 app.get("/", (req, res) => {
 	res.send("Backend is running");
 });
 app.use("/auth/facebook", facebookAuthRoute);
+app.use("/save", savedImageRoute);
 app.listen(port, () => {
 	new connectToDB();
 	console.log(`Server is running on port ${port}`);
